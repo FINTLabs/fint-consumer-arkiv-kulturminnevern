d83e09b4ed2b301fb49603dec42c1976fb593d0b
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2024-04-15 13:17:18 +0200

    new cache and fint relations

--- src/main/java/no/fint/consumer/models/dispensasjonautomatiskfredakulturminne/DispensasjonAutomatiskFredaKulturminneCacheService.java
+++ src/main/java/no/fint/consumer/models/dispensasjonautomatiskfredakulturminne/DispensasjonAutomatiskFredaKulturminneCacheService.java
@@ -127,7 +127,10 @@ public class DispensasjonAutomatiskFredaKulturminneCacheService extends CacheSer
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KulturminnevernActions.valueOf(event.getAction()) == KulturminnevernActions.UPDATE_DISPENSASJONAUTOMATISKFREDAKULTURMINNE) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<DispensasjonAutomatiskFredaKulturminneResource>> cacheObjects = data
--- src/main/java/no/fint/consumer/models/tilskuddfartoy/TilskuddFartoyCacheService.java
+++ src/main/java/no/fint/consumer/models/tilskuddfartoy/TilskuddFartoyCacheService.java
@@ -127,7 +127,10 @@ public class TilskuddFartoyCacheService extends CacheService<TilskuddFartoyResou
         } else {
             data = objectMapper.convertValue(event.getData(), javaType);
         }
-        data.forEach(linker::mapLinks);
+        data.forEach(resource -> {
+            linker.mapLinks(resource);
+            linker.resetSelfLinks(resource);
+        });
         if (KulturminnevernActions.valueOf(event.getAction()) == KulturminnevernActions.UPDATE_TILSKUDDFARTOY) {
             if (event.getResponseStatus() == ResponseStatus.ACCEPTED || event.getResponseStatus() == ResponseStatus.CONFLICT) {
                 List<CacheObject<TilskuddFartoyResource>> cacheObjects = data
