861cc241362922f363dc83b187e11cc0ce3cddd1
Trond Magnus Sevre <trond@fintlabs.no>
2024-05-07 13:41:05 +0200

    Fix problem with links and selflinks

--- src/main/java/no/fint/consumer/models/dispensasjonautomatiskfredakulturminne/DispensasjonAutomatiskFredaKulturminneController.java
+++ src/main/java/no/fint/consumer/models/dispensasjonautomatiskfredakulturminne/DispensasjonAutomatiskFredaKulturminneController.java
@@ -182,7 +182,7 @@ public class DispensasjonAutomatiskFredaKulturminneController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(dispensasjonautomatiskfredakulturminne);
+            return linker.mapAndResetLinks(dispensasjonautomatiskfredakulturminne);
         }    
     }
 
@@ -226,7 +226,7 @@ public class DispensasjonAutomatiskFredaKulturminneController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(dispensasjonautomatiskfredakulturminne);
+            return linker.mapAndResetLinks(dispensasjonautomatiskfredakulturminne);
         }    
     }
 
@@ -270,7 +270,7 @@ public class DispensasjonAutomatiskFredaKulturminneController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(dispensasjonautomatiskfredakulturminne);
+            return linker.mapAndResetLinks(dispensasjonautomatiskfredakulturminne);
         }    
     }
 
