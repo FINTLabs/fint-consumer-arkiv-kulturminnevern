febcb567fe60c87312c014536cb5e27b110b8dd0
Sander Nilsen Rysjedal <117441169+Sander14121@users.noreply.github.com>
2026-01-19 10:34:39 +0100

    Version 4.0.0-rc-6. (#34)
    * Version 4.0.0-rc-6.
    
    * refactor and add odata filter

---
+++ src/main/java/no/novari/fint/consumer/utils/RestEndpoints.java
@@ -0,0 +1,12 @@
+package no.novari.fint.consumer.utils;
+
+public enum RestEndpoints {
+    ;
+
+    public static final String ADMIN = "/admin";
+	public static final String DISPENSASJONAUTOMATISKFREDAKULTURMINNE = "/dispensasjonautomatiskfredakulturminne";
+	public static final String KLASSE = "/klasse";
+	public static final String TILSKUDDFARTOY = "/tilskuddfartoy";
+	public static final String TILSKUDDFREDABYGNINGPRIVATEIE = "/tilskuddfredabygningprivateie";
+
+}
